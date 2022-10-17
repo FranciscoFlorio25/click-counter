242da55f5ca26cc6d27bc1b1ee359efd67399297
Francisco Florio <fflorio@algeiba.com>
2022-10-17 10:48:23 -0300

    Botones ceados

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Button from './Components/Button';
 import CountLogo from './Image/CountIMG.png';
 
 
 function App() {
+
+  let count = 0;
+
+  const handleClick = ()  => {
+    count++;
+  }
+  const resetCount = () => {
+    count = 0;
+  }
+
   return (
     <div className="App">
       <div className='Logo-container'>
@@ -14,7 +25,16 @@ function App() {
         />
       </div>
       <div className='main-container'>
-        
+        <Button 
+          text='Click me!'
+          isClickButton={true}
+          handleClick={handleClick}
+        />
+        <Button
+          text='Reset'
+          isClickButton={true}
+          handleClick={resetCount}
+        />
       </div>
     </div>
   );
